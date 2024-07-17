5f3299220ff06fe9b56f7a3f336f2cf7b6bd2f5b
Yash Raj <89970907+yash-raj202134@users.noreply.github.com>
2024-07-17 19:59:36 +0530

    dashboard utility

---
+++ app.py
@@ -0,0 +1,17 @@
+import pickle
+from flask import request, Flask, render_template, redirect, url_for, flash #type:ignore
+
+from flask import request, Flask, render_template, redirect, url_for, flash #type:ignore
+
+
+import pandas as pd #type:ignore
+
+# ======================================Create app=================================================
+app = Flask(__name__)
+
+# ======================================loading models and datasets================================
+df = pd.read_csv('recruitment_system/dataset/HR_comma_sep.csv.crdownload')
+
+model = pickle.load(open('recruitment_system/models/model.pkl','rb'))
+scaler = pickle.load(open('recruitment_system/models/scaler.pkl','rb'))
+
